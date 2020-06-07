5e84bd76e7d1c0f048f592c670b34864c03d44f1
unknown <yeehawbw@gmail.com>
2020-06-07 11:58:43 -0400

    Different colored navtabs

--- src/components/Navtab.js
+++ src/components/Navtab.js
@@ -3,18 +3,47 @@ import { Link } from "gatsby"
 
 export default function Navtab({ name, dest, path }) {
   const inset = path === dest ? true : false
+  let background = ""
+  let gradient = ""
+  let insetShadows = ""
+
+  switch (true) {
+    case dest === "/" || dest === "/parents":
+      background = "#d92027"
+      gradient = "linear-gradient(145deg, #e8222a, #c31d23)"
+      insetShadows =
+        "inset 10px 10px 20px #570d10, inset -10px -10px 20px #ff333e"
+      break
+    case dest === "/books" || dest === "/research":
+      background = "#ff9234"
+      gradient = "linear-gradient(145deg, #ff9c38, #e6832f)"
+      insetShadows =
+        "inset 10px 10px 20px #96561f, inset -10px -10px 20px #ffce49"
+      break
+    case dest === "/students" || dest === "/tutorials":
+      background = "#ffcd3c"
+      gradient = "linear-gradient(145deg, #ffdb40, #e6b936)"
+      insetShadows =
+        "inset 10px 10px 20px #705a1a, inset -10px -10px 20px #ffff5e"
+      break
+    case dest === "/teachers" || dest === "/about":
+      background = "#1EC198"
+      gradient = "linear-gradient(145deg, #20cfa3, #1bae89)"
+      insetShadows =
+        "inset 10px 10px 20px #0c4d3d, inset -10px -10px 20px #30fff3"
+      break
+    default:
+  }
 
   return (
-    <Link to={dest} style={{ color: "#fff8f0", textDecoration: "none" }}>
+    <Link to={dest} style={{ textDecoration: "none" }}>
       <button
         className="banner-button"
         style={{
-          background: inset
-            ? "#3167ED"
-            : `linear-gradient(145deg, #346efe, #2c5dd5)`,
+          background: inset ? background : gradient,
           boxShadow: inset
-            ? "inset 7px 7px 18px #162d68, inset -7px -7px 18px #4ca1ff"
-            : "7px 7px 18px #2751bb, -7px -7px 18px #3b7dff",
+            ? insetShadows
+            : "10px 10px 20px #2751bb, -10px -10px 20px #3b7dff",
         }}
       >
         {name}
